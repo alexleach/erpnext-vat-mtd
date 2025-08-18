bd3edd49d368035053007a7551d0d261adb46656
Alex Leach <zander_leach@hotmail.com>
2025-08-18 05:50:03 +0000

    fix: Upgrade build system as per PEP 621

--- uk_vat/__init__.py
+++ uk_vat/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+"""United Kingdom VAT Return and Making-Tax-Digital (MTD) submission"""
 
-__version__ = '0.0.1'
+__version__ = "0.0.1"
 
