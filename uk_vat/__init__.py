18144d1663bcca744307c49bd98153651933bc35
Alex Leach <zander_leach@hotmail.com>
2025-08-23 04:37:31 +0000

    fix: flit build system couldn't find package docstring

--- uk_vat/__init__.py
+++ uk_vat/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 """United Kingdom VAT Return and Making-Tax-Digital (MTD) submission"""
+from __future__ import unicode_literals
 
 __version__ = "0.0.1"
 
